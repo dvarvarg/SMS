b950d76db475e3fb0a3d9b7b250324d301404d4d
dvarvarg <dvarvarg@gmail.com>
2024-10-31 12:31:28 +0300

    Добавлена проверка корректности номера с помощью регулярных выражений

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import requests
+import re
+
+
+def validate_phone_number(phone_number):
+    pattern=r'^\d{11}$'
+    return bool(re.match(pattern,phone_number))
+
+
 
 user='python24'
 password='TCMS9L'
@@ -7,16 +15,19 @@ reciever='79163439281' # получатель
 text='Hello world!'
 
 
-url=f'https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={reciever}&text={text}'
-print(url)
+if not validate_phone_number(reciever):
+    print('Ошибка! Некорректный номер телефона')
+else:
+    url=f'https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={reciever}&text={text}'
+    print(url)
 
-try:
-    response=requests.get(url)
-    print(response)
+    try:
+        response=requests.get(url)
+        print(response)
 
-    if response.status_code==200:
-        print('Сообщение успешно отправлено!')
-    else:
-        print(f'Ошибка при отправке {response.status_code}')
-except Exception as e:
-    print(f'Непредвиденная ошибка с кодом {e}')
+        if response.status_code==200:
+            print('Сообщение успешно отправлено!')
+        else:
+            print(f'Ошибка при отправке {response.status_code}')
+    except Exception as e:
+        print(f'Непредвиденная ошибка с кодом {e}')
